9cb26006efca36dadc353e848272d686b0e7d906
Muyang Shi <shimy17@163.com>
2018-09-16 01:09:32 -0500

    Mostly finished, the books/authors, and also the forward/reverse; Need to work on the error messages together, I spent a lot of time learning python on my own and I think it would be faster if we can work together

--- books/books1.py
+++ books/books1.py
@@ -5,9 +5,13 @@ from operator import itemgetter
 
 filename = sys.argv[1]
 action = sys.argv[2]
+#Sort Direction is optional, if the user does not specify, the program
+#will sort in forward direction
 try:
 	sortDirection = sys.argv[3]
 except:
+	sortDirection = "forward"
+	print("Sorted in forward alphabetical order")
 	pass
 
 books = []
@@ -17,13 +21,29 @@ for row in reader:
 	books.append(row)
 
 #figure out how to negate cases
-if action == "books":
-	books.sort()
-	for row in books:
-		print(row[0])
-elif action == "authors":
-	for row in books:
-		sortlist.append(row[2])
-	print(sortlist)
-	books.sort(key = itemgetter(2))
+if (sortDirection != "forward") and (sortDirection != "reverse"):
+	print("Usage: Optional [sort-direction] can be either forward or backward.")
+	print("		Please try again")
+else:
+	if action == "books":
+		if sortDirection == "forward":
+			books.sort()
+		elif sortDirection == "reverse":
+			books.sort(reverse = True)
+		for row in books:
+			print(row[0])
+		#else:
+		#	print("Usage: Optional [sort-direction] can be either forward or backward.")
+		#	print("		Please try again")
+	if action == "authors":
+		for row in books:
+			sortlist.append(row[2])
+		if sortDirection == "forward":
+			sortlist.sort(key = lambda x: x.split(" ")[-2])
+		elif sortDirection == "reverse":
+			sortlist.sort(key = lambda x: x.split(" ")[-2], reverse = True)
+		for author in sortlist:
+			print(''.join(names for names in author if names not in '()-1234567890'))
+
+input("\nPress the enter key to exit.")
 	
