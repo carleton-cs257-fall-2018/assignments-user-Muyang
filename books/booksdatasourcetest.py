af11ea193616ad091cc677e00c030fa9c3a9b197
Muyang Shi <shimy17@163.com>
2018-09-19 16:05:36 -0500

    Makefile added

--- books/booksdatasourcetest.py
+++ books/booksdatasourcetest.py
@@ -3,21 +3,21 @@
 	Muyang Shi, Justin T. Washington 18 Sept 2018
 '''
 
-import BooksDataSource
-import unitest
+import booksdatasource
+import unittest
 
-class booksdatasourcetest.py(unittest.TestCase):
+class booksdatasourcetest(unittest.TestCase):
 	def setUp(self):
-		self.books_data_source = BooksDataSource.booksdatasource('books.csv', 'authors.csv', 'books_authors.csv')
+		self.books_data_source = booksdatasource.BooksDataSource('books.csv', 'authors.csv', 'books_authors.csv')
 
 	def tearDown(self):
 		pass
 
 	def test_books(self):
-
+		pass
 	def test_authors(self):
-
+		pass
 	def test_books_authors_links(self):
-
+		pass
 if __name__ == '__main__':
 	unittest.main()
