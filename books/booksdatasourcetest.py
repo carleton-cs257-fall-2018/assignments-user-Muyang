8e0ef9cdbbc5fa36caa0f96706fa8feb4548c4ce
Muyang Shi <shimy17@163.com>
2018-09-18 23:45:16 -0500

    empty booksdatasourcetest.py

---
+++ books/booksdatasourcetest.py
@@ -0,0 +1,23 @@
+'''
+	booksdatasourcetest.py
+	Muyang Shi, 18 Sept 2018
+'''
+
+import BooksDataSource
+import unitest
+
+class booksdatasourcetest.py(unittest.TestCase):
+	def setUp(self):
+		self.
+
+	def tearDown(self):
+		pass
+
+	def test_books(self):
+
+	def test_authors(self):
+
+	def test_books_authors_links(self):
+
+if __name__ == '__main__':
+	unittest.main()
