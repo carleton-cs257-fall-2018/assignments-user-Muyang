5ea4d13213779f41d1d16ebdb39372e69f02cfec
Muyang Shi <shimy17@163.com>
2018-09-21 00:16:41 -0500

    More books tests

--- books/booksdatasourcetest.py
+++ books/booksdatasourcetest.py
@@ -16,15 +16,30 @@ class booksdatasourcetest(unittest.TestCase):
 			{'id':41,'title':'Middlemarch','publication-year':1871})
 	def test_book_no_id(self):
 		self.assertRaises(ValueError, self.books_data_source.book, -1)
+
 	def test_books_author_id(self):
 		self.assertEqual(self.books_data_source.books(author_id=22),
 			[{'id':41, 'title':'Middlemarch','publication-year':1871}])
+	def test_books_wrong_author_id(self):
+		self.assertRaises(ValueError,self.books_data_source.books, author_id=-1)
+
 	def test_books_search_text(self):
 		self.assertEqual(self.books_data_source.books(search_text='middle'),
 			[{'id':41, 'title':'Middlemarch','publication-year':1871}])
+
 	def test_books_start_year(self):
 		self.assertEqual(self.books_data_source.books(start_year=2016),
 			[{'id':35, 'title':'The Power', 'publication-year':2016}])
+	def test_books_wrong_start_year(self):
+		self.assertRaises(ValueError, self.books_data_source.books, start_year=-1)
+
+	def test_books_end_year(self):
+		self.assertEqual(self.books_data_source.books(end_year=1813),
+			[{'id':18, 'title':'Pride and Prejudice', 'publication-year':1813},
+			{'id':20, 'title':'Sense and Sensibility', 'publication-year':1813}])
+	def test_books_wrong_end_year(self):
+		self.assertRaises(ValueError,self.books_data_source.books,end_year=-1)
+
 	def test_author(self):
 		self.assertEqual(self.books_data_source.author(22), 
 			{'id':22, 'last_name':'Eliot', 'first_name':'George', 'birth_year':1949, 'death_year': None})
