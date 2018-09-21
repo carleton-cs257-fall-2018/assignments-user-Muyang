0bcc68d70cc85c4b1b4b9f6c2f4f968a2fa60492
Muyang Shi <shimy17@163.com>
2018-09-21 11:54:57 -0500

    Some tests around book methods

--- books/booksdatasourcetest.py
+++ books/booksdatasourcetest.py
@@ -11,12 +11,15 @@ class booksdatasourcetest(unittest.TestCase):
 		self.books_data_source = booksdatasource.BooksDataSource('books.csv', 'authors.csv', 'books_authors.csv')
 	def tearDown(self):
 		pass
+
+	#Testing the book(self, book_id) method
 	def test_book(self):
 		self.assertEqual(self.books_data_source.book(41),
 			{'id':41,'title':'Middlemarch','publication-year':1871})
 	def test_book_no_id(self):
 		self.assertRaises(ValueError, self.books_data_source.book, -1)
 
+	#Testing the books method
 	def test_books_author_id(self):
 		self.assertEqual(self.books_data_source.books(author_id=22),
 			[{'id':41, 'title':'Middlemarch','publication-year':1871}])
@@ -40,10 +43,17 @@ class booksdatasourcetest(unittest.TestCase):
 	def test_books_wrong_end_year(self):
 		self.assertRaises(ValueError,self.books_data_source.books,end_year=-1)
 
+	#Testing the books_for_author method
+	def books_for_author(self):
+		self.assertEqual(self.books_data_source.books_for_author(22),
+			[{'id':41, 'title':'Middlemarch', 'publication-year':1871}])
+	def books_for_author_wrong_id(self):
+		self.assertRaises(self.books_data_source.books_for_author, -1)
+
 	def test_author(self):
 		self.assertEqual(self.books_data_source.author(22), 
 			{'id':22, 'last_name':'Eliot', 'first_name':'George', 'birth_year':1949, 'death_year': None})
-	def test_books_authors_links(self):
-		pass
+	#def test_books_authors_links(self):
+	#	pass
 if __name__ == '__main__':
 	unittest.main()
