d718f4e3b6a37af430f0d7a7bcfa78e4d8a1e94c
Muyang Shi <shimy17@163.com>
2018-09-20 20:47:24 -0500

    Python Style

--- books/booksdatasourcetest.py
+++ books/booksdatasourcetest.py
@@ -12,11 +12,14 @@ class booksdatasourcetest(unittest.TestCase):
 	def tearDown(self):
 		pass
 	def test_book(self):
-		self.assertEqual(self.books_data_source.book(41),{'id':41,'title':'Middlemarch','publication-year':1871})
+		self.assertEqual(self.books_data_source.book(41),
+			{'id':41,'title':'Middlemarch','publication-year':1871})
 	def test_books(self):
-		self.assertEqual(self.books_data_source.books(author_id=22),[{'id':41, 'title':'Middlemarch','publication-year':1871}])
-	def test_authors(self):
-		self.assertEqual(self.books_data_source.author(22), {'id':22, 'last_name':'Eliot', 'first_name':'George', 'birth_year':1949, 'death_year': None})
+		self.assertEqual(self.books_data_source.books(author_id=22),
+			[{'id':41, 'title':'Middlemarch','publication-year':1871}])
+	def test_author(self):
+		self.assertEqual(self.books_data_source.author(22), 
+			{'id':22, 'last_name':'Eliot', 'first_name':'George', 'birth_year':1949, 'death_year': None})
 	def test_books_authors_links(self):
 		pass
 if __name__ == '__main__':
