640d48609b3bdaa850f888d4efe32fa84dc0a6bf
Muyang Shi <shimy17@163.com>
2018-09-20 20:29:39 -0500

    A start on booksdatasourcetest.py

--- books/booksdatasourcetest.py
+++ books/booksdatasourcetest.py
@@ -9,14 +9,14 @@ import unittest
 class booksdatasourcetest(unittest.TestCase):
 	def setUp(self):
 		self.books_data_source = booksdatasource.BooksDataSource('books.csv', 'authors.csv', 'books_authors.csv')
-
 	def tearDown(self):
 		pass
-
+	def test_book(self):
+		self.assertEqual(self.books_data_source.book(41),{'id':41,'title':'Middlemarch','publication-year':1871})
 	def test_books(self):
-		pass
+		self.assertEqual(self.books_data_source.books(author_id=22),[{'id':41, 'title':'Middlemarch','publication-year':1871}])
 	def test_authors(self):
-		pass
+		self.assertEqual(self.books_data_source.author(22), {'id':22, 'last_name':'Eliot', 'first_name':'George', 'birth_year':1949, 'death_year': None})
 	def test_books_authors_links(self):
 		pass
 if __name__ == '__main__':
