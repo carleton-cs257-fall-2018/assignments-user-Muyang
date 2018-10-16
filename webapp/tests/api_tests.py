d5594c53e534ad7bafc32cc194fa5ae37cb3c3d9
Muyang Shi <shimy17@163.com>
2018-10-16 15:40:26 -0500

    Muyang's work on unittests

--- webapp/tests/api_tests.py
+++ webapp/tests/api_tests.py
@@ -54,20 +54,39 @@ class SchoogleTests(unittest.TestCase):
 		fetched_list = json.loads(string_from_server)
 		self.assertEqual(fetched_list, [{}])
 
-	def test_major(self):
+	def test_major_searched(self):
 		test_url = BASE_URL + "schools?history=true&name=carleto&fields=name,city"
 		data_from_server = urllib.request.urlopen(test_url).read()
 		string_from_server = data_from_server.decode('utf-8')
 		fetched_list = json.loads(string_from_server)
 		self.assertEqual(fetched_list, [{'name':'Carleton College', 'city': 'Northfield'}])
+	def test_major_in_fields(self):
+		test_url = BASE_URL + "schools?name=carleto&fields=name,city,Agriculture"
+		data_from_server = urllib.request.urlopen(test_url).read()
+		string_from_server = data_from_server.decode('utf-8')
+		fetched_list = json.loads(string_from_server)
+		self.assertEqual(fetched_list, [{'name':'Carleton College', 'city': 'Northfield','Agriculture':'Not Offered'}])
 
 
+	def test_school_id_invalid(self):
+		#test_url = BASE_URL + "schools?id=-1"
+		self.assertRaises(ValueError, school_searched_by_id, -1)
+		#test_url_2 = BASE_URL + "schools?id=1.5"
+		self.assertRaises(TypeError, school_searched_by_id, 1.5)
 	def test_state_id_invalid(self):
-		test_url = BASE_URL + "schools?id=-1"
-		self.assertRaises(ValueError, urllib.request.urlopen, test_url)
+		#test_url = BASE_URL + "states?id=-1"
+		self.assertRaises(ValueError, state_searched_by_id, -1)
+		#test_url_2 = BASE_URL + "states?id=1.5"
+		self.assertRaises(TypeError, state_searched_by_id, 1.5)
 
 
-	def test_no_results(self):
+	def test_school_id_no_results(self):
+		test_url = BASE_URL + "schools?id=999999"
+		data_from_server = urllib.request.urlopen(test_url).read()
+		string_from_server = data_from_server.decode('utf-8')
+		fetched_list = json.loads(string_from_server)
+		self.assertEqual(fetched_list, [{}])
+	def test_enrollment_no_results(self):
 		test_url = BASE_URL + "schools?enrollment=999999"
 		data_from_server = urllib.request.urlopen(test_url).read()
 		string_from_server = data_from_server.decode('utf-8')
