0a218b284ba17dcaf8742807eeaaa3935d4cc95d
JohnnyR <JohnnyReichman@eduroam-170-56.dyn.carleton.edu>
2018-10-16 13:40:07 -0500

    added some more tests, still all errors

--- webapp/tests/api_tests.py
+++ webapp/tests/api_tests.py
@@ -1,7 +1,6 @@
 '''
-	Phase 2 of the Books homework assignment for CS 257, Jeff Ondich
-	This program runs a variety of unit tests for booksdatasource.py
-	Created by Eric Stadelman and Johnny Reichman, 9/21/18
+	Unit Tests for Schoogle API
+	Created by Muyang Shi and Johnny Reichman, 10/16/18
 '''
 
 import sys
@@ -14,9 +13,42 @@ import unittest
 BASE_URL = "http://whatever.com/"
 
 class SchoogleTests(unittest.TestCase):
+
+	def test_name(self):
+		test_url = BASE_URL + "schools?name=carleto&fields=name,city"
+		ata_from_server = urllib.request.urlopen(test_url).read()
+		string_from_server = data_from_server.decode('utf-8')
+		fetched_list = json.loads(string_from_server)
+		self.assertEqual(fetched_list, [{'name':'Carleton College', 'city': 'Northfield'}])	
+
+
+	def test_admission(self):
+		test_url = BASE_URL + "schools?admission_rate=0.2..0.5&name=carleto&fields=name,city"
+		ata_from_server = urllib.request.urlopen(test_url).read()
+		string_from_server = data_from_server.decode('utf-8')
+		fetched_list = json.loads(string_from_server)
+		self.assertEqual(fetched_list, [{'name':'Carleton College', 'city': 'Northfield'}])
+
+
+	def test_major(self):
+		test_url = BASE_URL + "schools?history=true&name=carleto&fields=name,city"
+		ata_from_server = urllib.request.urlopen(test_url).read()
+		string_from_server = data_from_server.decode('utf-8')
+		fetched_list = json.loads(string_from_server)
+		self.assertEqual(fetched_list, [{'name':'Carleton College', 'city': 'Northfield'}])
+
+
 	def test_state_id_invalid(self):
 		test_url = BASE_URL + "schools?id=-1"
 		self.assertRaises(ValueError, urllib.request.urlopen, test_url)
+
+
+	def test_no_results(self):
+		test_url = BASE_URL + "schools?enrollment=999999"
+		data_from_server = urllib.request.urlopen(test_url).read()
+		string_from_server = data_from_server.decode('utf-8')
+		fetched_list = json.loads(string_from_server)
+		self.assertEqual(fetched_list, [])
 
 
 if __name__ == '__main__':
