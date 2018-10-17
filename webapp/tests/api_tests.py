93a80beb1e693b6e585a18a4da425919a200ef01
Muyang Shi <shimy17@163.com>
2018-10-16 19:34:02 -0500

    Adding tests to unittest

--- webapp/tests/api_tests.py
+++ webapp/tests/api_tests.py
@@ -33,7 +33,20 @@ class SchoogleTests(unittest.TestCase):
 		data_from_server = urllib.request.urlopen(test_url).read()
 		string_from_server = data_from_server.decode('utf-8')
 		fetched_list = json.loads(string_from_server)
-		self.assertEqual(fetched_list, [{}])	
+		self.assertEqual(fetched_list, [{}])
+
+	def test_school_by_ACT(self):
+		test_url = BASE_URL + "schools?ACT_cumulative_MID=30..34&name=carleto&fields=name,city"
+		data_from_server = urllib.request.urlopen(test_url).read()
+		string_from_server = data_from_server.decode('utf-8')
+		fetched_list = json.loads(string_from_server)
+		self.assertEqual(fetched_list, [{'name':'Carleton College', 'city': 'Northfield'}])
+	def test_school_by_SAT(self):	
+		test_url = BASE_URL + "schools?SAT_average=1400..1500&name=carleto&fields=name,city"
+		data_from_server = urllib.request.urlopen(test_url).read()
+		string_from_server = data_from_server.decode('utf-8')
+		fetched_list = json.loads(string_from_server)
+		self.assertEqual(fetched_list, [{'name':'Carleton College', 'city': 'Northfield'}])
 
 	def test_highest_degree(self):
 		test_url = BASE_URL + "schools?name=carleto&fields=name,city,highest_degree"
@@ -68,20 +81,21 @@ class SchoogleTests(unittest.TestCase):
 		self.assertEqual(fetched_list, [{'name':'Carleton College', 'city': 'Northfield','Agriculture':'Not Offered'}])
 
 
+	#There should be errors
+	def test_school_name_invalid(self):
+		self.assertRaises(TypeError, school_searched_by_name, 3.1415926)
 	def test_school_id_invalid(self):
-		#test_url = BASE_URL + "schools?id=-1"
 		self.assertRaises(ValueError, school_searched_by_id, -1)
-		#test_url_2 = BASE_URL + "schools?id=1.5"
 		self.assertRaises(TypeError, school_searched_by_id, 1.5)
-	def test_state_id_invalid(self):
-		#test_url = BASE_URL + "states?id=-1"
-		self.assertRaises(ValueError, state_searched_by_id, -1)
-		#test_url_2 = BASE_URL + "states?id=1.5"
-		self.assertRaises(TypeError, state_searched_by_id, 1.5)
 	def test_school_major_invalid(self):
 		self.assertRaises(TypeError, school_searched_by_major, 1)
 
+	def test_state_id_invalid(self):
+		self.assertRaises(ValueError, state_searched_by_id, -1)
+		self.assertRaises(TypeError, state_searched_by_id, 1.5)
 
+
+	#There should be no results
 	def test_school_id_no_results(self):
 		test_url = BASE_URL + "schools?id=999999"
 		data_from_server = urllib.request.urlopen(test_url).read()
@@ -94,6 +108,12 @@ class SchoogleTests(unittest.TestCase):
 		string_from_server = data_from_server.decode('utf-8')
 		fetched_list = json.loads(string_from_server)
 		self.assertEqual(fetched_list, [{}])
+	def test_major_no_results(self):
+		test_url = BASE_URL + "schools?sleeping=true"
+		data_from_server = urllib.request.urlopen(test_url).read()
+		string_from_server = data_from_server.decode('utf-8')
+		fetched_list = json.loads(string_from_server)
+		self.assertEqual(fetched_list, [{}])		
 
 
 if __name__ == '__main__':
