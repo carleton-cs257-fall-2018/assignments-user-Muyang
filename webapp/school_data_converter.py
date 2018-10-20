59855f41d9e13c7baff7452ffabfb0971ad9e980
JohnnyR <JohnnyReichman@Jonathans-MacBook-Air.local>
2018-10-20 12:26:48 -0500

    state conversions working

--- webapp/school_data_converter.py
+++ webapp/school_data_converter.py
@@ -6,6 +6,71 @@ import re
 import csv
 
 
+
+state_match = {
+'1': 'Alabama',
+'2': 'Alaska',
+'4': 'Arizona',
+'5': 'Arkansas',
+'6': 'California',
+'8': 'Colorado',
+'9': 'Connecticut',
+'10': 'Delaware',
+'11': 'District of Columbia',
+'12': 'Florida',
+'13': 'Georgia',
+'15': 'Hawaii',
+'16': 'Idaho',
+'17': 'Illinois',
+'18': 'Indiana',
+'19': 'Iowa',
+'20': 'Kansas',
+'21': 'Kentucky',
+'22': 'Louisiana',
+'23': 'Maine',
+'24': 'Maryland',
+'25': 'Massachusetts',
+'26': 'Michigan',
+'27': 'Minnesota',
+'28': 'Mississippi',
+'29': 'Missouri',
+'30': 'Montana',
+'31': 'Nebraska',
+'32': 'Nevada',
+'33': 'New Hampshire',
+'34': 'New Jersey',
+'35': 'New Mexico',
+'36': 'New York',
+'37': 'North Carolina',
+'38': 'North Dakota',
+'39': 'Ohio',
+'40': 'Oklahoma',
+'41': 'Oregon',
+'42': 'Pennsylvania',
+'44': 'Rhode Island',
+'45': 'South Carolina',
+'46': 'South Dakota',
+'47': 'Tennessee',
+'48': 'Texas',
+'49': 'Utah',
+'50': 'Vermont',
+'51': 'Virginia',
+'53': 'Washington',
+'54': 'West Virginia',
+'55': 'Wisconsin',
+'56': 'Wyoming',
+'60': 'American Samoa',
+'64': 'Federated States of Micronesia',
+'66': 'Guam',
+'68': 'Marshal Islands',
+'69': 'Northern Mariana Islands',
+'70': 'Palau',
+'72': 'Puerto Rico',
+'78': 'Virgin Islands',
+'ST_FIPS': 'State Name'
+}
+
+
 def convert_int_text(metric, value):
 	if metric == 'highest_degree':
 		if value == 0:
@@ -93,11 +158,25 @@ def load_school(csv_file_name):
 def load_school_stats(csv_file_name):
 	pass
 def load_state(csv_file_name):
-	pass
-	#column18(ST_FIPS) is INT state ID, then add abbreviation and full name
+	csv_file = open(csv_file_name)
+	reader = csv.reader(csv_file)
+
+	states = []
+	state = {}
+	for row in reader:
+		assert len(row) == 1847
+		state = {
+		'state_id': row[17],
+		'state_name': state_match[row[17]],
+		'state_abbreviation': row[5],
+		}
+		states.append(state)
+	csv_file.close()
+	return states
 
 def main():
-	print(load_school('MERGED2016_17_PP.csv'))
+	print(load_state('MERGED2016_17_PP.csv'))
+
 
 
 if __name__ == '__main__':
@@ -107,3 +186,6 @@ if __name__ == '__main__':
 	# save_books_table(books, 'books.csv')
 	# save_authors_table(authors, 'authors.csv')
 	# save_linking_table(books_authors, 'books_authors.csv')
+
+
+
