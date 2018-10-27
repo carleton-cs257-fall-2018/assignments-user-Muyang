0b57fa79d930745fee2c7ee2711a3f4c322302ac
user-Muyang <shimy17@163.com>
2018-10-27 16:22:59 -0500

    10/27

--- webapp/static/Schoogle_Home.js
+++ webapp/static/Schoogle_Home.js
@@ -11,6 +11,26 @@ function onSearchButtonPress() {
 	fetch(url, {method: 'get'})
 
 	.then((response) => response.json())
+
+	.then(function(schoolsList) {
+        // Build the table body.
+        var tableBody = '';
+        for (var k = 0; k < schoolsList.length; k++) {
+            tableBody += '<tr>';
+
+            tableBody += '<td><a onclick="getAuthor(' + schoolsList[k]['school_id'] + ",'"
+                            + schoolsList[k]['school_name'] + '</a></td>';
+            tableBody += '<td>' + schoolsList[k]['city'] + '-';
+            tableBody += '</td>';
+            tableBody += '</tr>';
+        }
+
+        // Put the table body we just built inside the table that's already on the page.
+        var schools = document.getElementById('results_table');
+        if (schools) {
+            schools.innerHTML = tableBody;
+        }
+    })
 }
 
 function initialize() {
@@ -21,7 +41,7 @@ function initialize() {
 }
 
 function getBaseURL() {
-	var baseURL = window.location.protocol + '//' + window.location.hostname + ':' + '5001';
+	var baseURL = window.location.protocol + '//' + window.location.hostname + ':' + api_port;
 	return baseURL;
 }
 
