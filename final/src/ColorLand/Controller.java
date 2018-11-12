a1a5cfd264e8eeaeaedf6cf2f13dd557fe3f371c
Muyang <shimy17@163.com>
2018-11-11 19:46:34 -0600

    Adjust frame rate

--- final/src/ColorLand/Controller.java
+++ final/src/ColorLand/Controller.java
@@ -21,7 +21,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Controller implements EventHandler<KeyEvent> {
-    final private double FRAMES_PER_SECOND = 40.0;
+    final private double FRAMES_PER_SECOND = 20.0;
     private Model model;
     private String keyPressed;
     private Timer timer;
